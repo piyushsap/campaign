c82383f0d0193fd9a3cac05197e7b5d8319c8f0a
Piyush Kumar <piyushkumar.verma@sapien.com>
2019-09-13 04:58:30 +0530

    fix bugs

--- src/Common/SidebarRight.jsx
+++ src/Common/SidebarRight.jsx
@@ -4,7 +4,6 @@ import Properties from './Properties';
 
 
 function SidebarRight(props) {
-    const componentTypes = ['Text', 'Button', 'Input', 'Image', 'Video', 'Text', 'Image', 'Row', 'Divider'];
     // const [component, setComponent] = useState('Row');
     // useEffect(_ => {
     //     componentService.addComponentChangeSubscriber(data => {
