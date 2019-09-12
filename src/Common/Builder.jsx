08a2d0e2ba2f554a24bcf4f71a63dc4ed7f374b1
Jugal Joshi <jugjoshi@publicisgroupe.net>
2019-09-12 18:27:34 +0530

    builder changes

--- src/Common/Builder.jsx
+++ src/Common/Builder.jsx
@@ -4,42 +4,7 @@ import componentService from './../services/ComponentsService';
 import { NavLink } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import SidebarRight from './SidebarRight';
-
-
-function ComponentWrapper(props) {
-  let compParent = React.createRef();
-  const handleDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const handleDragEnter = (e) => {
-    compParent.current.classList.add('hover');
-  };
-
-  const handleDragLeave = (e) => {
-    compParent.current.classList.remove('hover');
-  };
-
-  const handleClick = () => {
-    document.querySelectorAll('.component-container').forEach(ele => {
-      ele.classList.remove('select');
-    });
-    compParent.current.classList.add('select');
-
-    props.clickHandler();
-
-  };
-
-  return (
-    <div ref={compParent} onDragOver = {(e) => handleDragOver(e)} onClick = {handleClick}
-    onDragEnter = {(e) => handleDragEnter(e)} 
-    onDragLeave = {(e) => handleDragLeave(e)} 
-    onDrop =  {(e) => handleDragLeave(e)} 
-    className ="component-container">
-      {props.children}
-    </div>
-  )
-};
+import ComponentWrapper from './ComponentWrapper';
 
 
 export const componentMap = {
